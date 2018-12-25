a7188c37996c238212293f99bf2c2af648ecd63e
dute7liang <31098086+dute7liang@users.noreply.github.com>
2018-12-25 15:34:32 +0800

    继续完善core包

---
+++ user/src/main/java/com/duteliang/user/repository/dao/UserTMapper.java
@@ -0,0 +1,16 @@
+package com.duteliang.user.repository.dao;
+
+import com.duteliang.user.repository.model.UserT;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 用户表 Mapper 接口
+ * </p>
+ *
+ * @author zl
+ * @since 2018-12-25
+ */
+public interface UserTMapper extends BaseMapper<UserT> {
+
+}
